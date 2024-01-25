8c70d618c25fb0dafdedce7cb89f7d4943f528b0
PhutiCee <72173500+PhutiCee@users.noreply.github.com>
2024-01-25 14:13:09 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto p-2">
+      <h1 className="font-bold text-xl">Blogs</h1>
+    </main>
+  );
+}
