0161464b11ccad077f888417868ef2ec0b38ae30
PhutiCee <72173500+PhutiCee@users.noreply.github.com>
2024-02-05 13:26:09 +0200

    Navbar color fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
         // </header>
 
         <div>
-            <nav className="bg-gray-600 border-gray-200 shadow-md dark:bg-gray-300">
+            <nav className="bg-gray-300 border-gray-200 shadow-md ">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 md:max-w-4xl lg:max-w-5xl">
                     <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse" onClick={closeMobileMenu}>
                         <h2 className="self-center text-2xl font-bold whitespace-nowrap">Blog<span className='text-blue-500'>IT</span></h2>
@@ -52,7 +52,7 @@ export default function Navbar() {
                         </svg>
                     </button>
                     <div className={`w-full md:block md:w-auto ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-300 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-300 md:dark:bg-gray-300 dark:border-gray-700">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-300 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-300 md:dark:bg-gray-300">
                             <li>
                                 <Link href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:bg-transparent" onClick={closeMobileMenu}>
                                     Home
