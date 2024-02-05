c00597c36bfcbba9ed6d3654f638e2088a00672a
PhutiCee <72173500+PhutiCee@users.noreply.github.com>
2024-02-05 14:10:03 +0200

    added facebook link

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -80,7 +80,7 @@ export default function page() {
                             </ul>
                         </div>
                     </div>
-                </div>
+                </div> 
             </div>
         </section>
     )
